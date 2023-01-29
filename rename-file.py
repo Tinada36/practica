70370947c284ab2bdf191243b30099487e49b516
Tinada36 <gruzin_apelsin@mail.ru>
2023-01-29 16:24:24 +0300

    Create rename-file.py

---
+++ rename-file.py
@@ -0,0 +1,16 @@
+import os
+
+def rename_files(directory, pattern, kolvo):
+    for i in range(kolvo):
+        file_path = os.path.join(directory, os.listdir(directory)[i])
+        print(file_path)
+        if os.path.isfile(file_path):
+            new_filename = pattern.format(os.listdir(directory)[i])
+            new_file_path = os.path.join(directory, new_filename)
+            os.rename(file_path, new_file_path)
+
+
+kolvo = int(input("Введите сколько файлов переименовать: "))
+directory = input("Enter the directory path: ")
+pattern = input("Enter the file renaming pattern (e.g. '{}_new'): ")
+rename_files(directory, pattern, kolvo)
